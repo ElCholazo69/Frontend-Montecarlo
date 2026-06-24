6538c18864780996be3f77ebc6e78fe86371e346
Enzo <U23203519@utp.edu.pe>
2026-06-23 21:38:41 -0500

    Pagina principal

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,10 +2,11 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Navbar } from "./shared/navbar/navbar";
 import { Footer } from "./shared/footer/footer";
+import { Inicio } from "./features/pages/inicio/inicio";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Navbar, Footer],
+  imports: [RouterOutlet, Navbar, Footer, Inicio],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
