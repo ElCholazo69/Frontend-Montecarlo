22509cf2079fbab06fff14277895ad67d83ada34
Enzo <U23203519@utp.edu.pe>
2026-06-26 03:48:18 -0500

    Validacion de formulario de 'Registro de Usuario'

--- src/app/features/auth/registro/registro.ts
+++ src/app/features/auth/registro/registro.ts
@@ -1,12 +1,33 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms';
 import { RouterLink } from '@angular/router';
+
 
 @Component({
   selector: 'app-registro',
-  imports: [RouterLink],
+  imports: [RouterLink, ReactiveFormsModule],
   templateUrl: './registro.html',
   styleUrl: './registro.scss',
 })
 export class Registro {
+  formularioRegistro: FormGroup
 
+  constructor(private fb: FormBuilder) {
+    this.formularioRegistro = this.fb.group({
+      nombre: ['', Validators.required],
+      apellido: ['', Validators.required],
+      correo: ['', [Validators.required, Validators.email]],
+      telefono: ['', [Validators.required, Validators.pattern('^[9][0-9]{8}$')]],
+      password: ['', [Validators.required, Validators.minLength(6)]]
+    });
+  }
+
+  onSubmit() {
+    if (this.formularioRegistro.valid) {
+      console.log(this.formularioRegistro.value);
+
+    } else {
+      this.formularioRegistro.markAllAsTouched();
+    }
+  }
 }
