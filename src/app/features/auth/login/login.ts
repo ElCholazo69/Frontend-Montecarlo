a4e642340c4a2b34d9f745a9fd250f6431401d02
Enzo <U23203519@utp.edu.pe>
2026-06-26 04:08:10 -0500

    Validacion de formulario de 'Login de Usuario'

--- src/app/features/auth/login/login.ts
+++ src/app/features/auth/login/login.ts
@@ -1,12 +1,29 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-login',
-  imports: [RouterLink],
+  imports: [RouterLink, ReactiveFormsModule],
   templateUrl: './login.html',
   styleUrl: './login.scss',
 })
-export class Login {
+export class Login { 
+  formularioLogin: FormGroup;
 
+  constructor(private fb: FormBuilder) {
+    this.formularioLogin = this.fb.group({
+      correo: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]]
+    });
+  }
+
+  onSubmit() {
+    if (this.formularioLogin.valid) {
+      console.log(this.formularioLogin.value);
+
+    } else {
+      this.formularioLogin.markAllAsTouched();
+    }
+  }
 }
