e08f0e7becfe0cf67bfdce4b4d4d57c302424de5
ElCholazo69 <illia_liberato99@hotmail.com>
2026-07-10 16:48:44 -0500

    Trabajo guardado-footer e animacion

--- src/app/features/pages/inicio/inicio.ts
+++ src/app/features/pages/inicio/inicio.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { RouterLink,Router } from '@angular/router';
+import { Cancha } from '../../../models/cancha';
+import { CanchaService } from '../../../services/cancha.service';
+import { AuthService } from '../../../services/auth.service';
 
 @Component({
   selector: 'app-inicio',
@@ -8,5 +11,39 @@ import { RouterLink } from '@angular/router';
   styleUrl: './inicio.scss',
 })
 export class Inicio {
+  canchasDestacadas: Cancha[] = []
+  imagen: string = "https://static.vecteezy.com/system/resources/thumbnails/000/104/368/small/free-soccer-field-vector.jpg"
+
+  constructor(private canchaService: CanchaService, private router: Router, private authService:AuthService){}
+
+  ngOnInit(): void{
+    this.canchaService.listarCanchas().subscribe({
+      next: (canchas) =>{
+        this.canchasDestacadas = canchas.slice(-5).reverse()
+      },
+      error: (err) => {
+        console.error(err)
+      }
+    });
+  }
+
+  reservar(cancha: Cancha): void {
+    if (!this.authService.logueadoSignal()) {
+
+      this.router.navigate(['/login'], {
+        queryParams: {
+          mensaje: 'reserva'
+        }
+      });
+      return;
+    }
+
+    const imagen = this.imagen
+    this.router.navigate(['/Dcanchas'], {
+      state: {
+        reserva: { cancha, imagen}
+      }
+    });
+  }
 
 }
